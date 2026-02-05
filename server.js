20c1240567e7824fcb3de1ca7ae7c1afc82712ac
ArmenT83 <armox625@gmail.com>
2026-02-05 22:05:36 +0400

    test: removed app.get to fix crash

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Սա ապահովում է, որ ցանկացած անհայտ հարցման դեպքում index.html-ը բացվի
-app.get('(.*)', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Սերվերը միացավ: Port ${PORT}`);
